69d0e79278a10520cf5555e78652f2ca71835d36
Fran Rodriguez <fran.rodriguez@devo.com>
2020-12-11 11:40:37 +0100

    Fix test

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -9,13 +9,13 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 def test_nodejs_version8(host):
     ansible_vars = host.ansible.get_variables()
-    nodejs_version = ansible_vars.get('nodejs_version8')
-    nodejs_output = host.run('/opt/nvm/bin/node -version')
-    assert str(nodejs_version) in nodejs_output.stderr
+    nodejs_version = "v" + ansible_vars.get('nodejs_version8')
+    nodejs_output = os.listdir('/opt/nvm/versions/node/')
+    assert str(nodejs_version) in nodejs_output
 
 
 def test_nodejs_version12(host):
     ansible_vars = host.ansible.get_variables()
-    nodejs_version = ansible_vars.get('nodejs_version12')
-    nodejs_output = host.run('/opt/nvm/bin/node -version')
-    assert str(nodejs_version) in nodejs_output.stderr
+    nodejs_version = "v" + ansible_vars.get('nodejs_version12')
+    nodejs_output = os.listdir('/opt/nvm/versions/node/')
+    assert str(nodejs_version) in nodejs_output
